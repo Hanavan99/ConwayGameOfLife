5420a1f89461f931ea911adf8690091e6ecb2bcb
Nathan Gawith <NathanGawith@gmail.com>
2016-04-22 15:48:53 -0500

    Added some buttons to MainPanel http://puu.sh/os3NI/20d6ad18ae.jpg

--- ConwayGameOfLife/src/com/conway/panels/MainPanel.java
+++ ConwayGameOfLife/src/com/conway/panels/MainPanel.java
@@ -1,11 +1,12 @@
 package com.conway.panels;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
 /**
  * The main menu panel.
  * 
- * @author Hanavan Kuhn
+ * @author Hanavan Kuhn, Nathan Gawith
  *
  */
 public class MainPanel extends AbstractPanel {
@@ -15,10 +16,30 @@ public class MainPanel extends AbstractPanel {
 	public MainPanel() {
 		setBounds(0, 0, 1920, 1080);
 		JLabel title = new JLabel("Conway's Game Of Life");
-		getUIManager().getUIConfig("title").applyBoundsToComponent(title);
-		title.setFont(getUIManager().getUIConfig("title").getFont());
+		getUIManager().getUIConfig("main_title").applyBoundsToComponent(title);
+		title.setFont(getUIManager().getUIConfig("main_title").getFont());
 		title.setHorizontalAlignment(JLabel.CENTER);
 		add(title);
+		JButton multiCreate = new JButton("Create Multiplayer Game");
+		JButton multiJoin = new JButton("Join Multiplayer Game");
+		JButton singleCreate = new JButton("Singleplayer Game");
+		JButton exit = new JButton("Exit");
+		getUIManager().getUIConfig("main_button1").applyBoundsToComponent(title);
+		multiCreate.setFont(getUIManager().getUIConfig("main_button1").getFont());
+		multiCreate.setHorizontalAlignment(JLabel.CENTER);
+		add(multiCreate);
+		getUIManager().getUIConfig("main_button2").applyBoundsToComponent(title);
+		multiJoin.setFont(getUIManager().getUIConfig("main_button2").getFont());
+		multiJoin.setHorizontalAlignment(JLabel.CENTER);
+		add(multiCreate);
+		getUIManager().getUIConfig("main_button3").applyBoundsToComponent(title);
+		singleCreate.setFont(getUIManager().getUIConfig("main_button3").getFont());
+		singleCreate.setHorizontalAlignment(JLabel.CENTER);
+		add(singleCreate);
+		getUIManager().getUIConfig("main_button4").applyBoundsToComponent(title);
+		singleCreate.setFont(getUIManager().getUIConfig("main_button4").getFont());
+		singleCreate.setHorizontalAlignment(JLabel.CENTER);
+		add(singleCreate);
 	}
 	
 }
