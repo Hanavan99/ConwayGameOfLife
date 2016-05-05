62e316bc38827c1651df38996abdee3c237220a4
Zach Deibert <zachdeibert@gmail.com>
2016-05-05 16:07:38 -0500

    Cleaned up logging some more

--- src/main/java/com/conway/logging/LogManager.java
+++ src/main/java/com/conway/logging/LogManager.java
@@ -9,44 +9,28 @@ import com.guilogger.logging.LogType;
 import com.guilogger.util.LoggingException;
 
 public class LogManager {
-
 	private static GUILogger logger;
-	private static boolean uselogger = true;
 
-	public static void init() {
-		if (uselogger == false) {
-			System.out.println("Logger flag set to false, canceling init");
-			return;
-		}
+	static {
 		logger = new GUILogger("Console");
 		try {
 			logger.addLogType("trace", new LogType("TRACE", 1, new Color(127, 127, 191)));
-			logger.addLogType("debug", new LogType("DEBUG", 2, new Color(0, 255, 63)));
+			logger.addLogType("debug", new LogType("DEBUG", 2, new Color(0, 191, 63)));
 			logger.addLogType("info", new LogType("INFO", 3, new Color(0, 0, 0)));
-			logger.addLogType("warn", new LogType("WARN", 4, new Color(255, 191, 0)));
+			logger.addLogType("warn", new LogType("WARN", 4, new Color(191, 127, 0)));
 			logger.addLogType("error", new LogType("ERROR", 5, new Color(255, 0, 0)));
 			logger.addLogType("fatal", new LogType("FATAL", 6, new Color(191, 0, 0)));
-		} catch (LoggingException e) {
-			System.out.println("Could not initialize logger");
+		} catch ( LoggingException e ) {
+			System.err.println("Could not initialize logger");
+			e.printStackTrace();
 		}
 	}
 
 	public static void log(String type, String message) {
-		if (uselogger) {
-			logger.log(message, type);
-		} else {
-			System.out.println(message);
-		}
-	}
-	
-	public static void log(LogEvent event) {
-		if ( uselogger ) {
-			logger.log(event.getMessage().getFormattedMessage(), event.getLevel().name().toLowerCase());
-		}
-	}
-	
-	public static void useLogger(boolean flag) {
-		uselogger = flag;
+		logger.log(message, type);
 	}
 
+	public static void log(LogEvent event) {
+		logger.log(event.getMessage().getFormattedMessage(), event.getLevel().name().toLowerCase());
+	}
 }
