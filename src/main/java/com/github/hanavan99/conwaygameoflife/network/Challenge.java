ea0974c76205df33bf6275121de7c381f2480944
Zach Deibert <zachdeibert@gmail.com>
2016-05-09 16:02:42 -0500

    Added challenge implementation

--- src/main/java/com/github/hanavan99/conwaygameoflife/network/Challenge.java
+++ src/main/java/com/github/hanavan99/conwaygameoflife/network/Challenge.java
@@ -1,6 +1,11 @@
 package com.github.hanavan99.conwaygameoflife.network;
 
+import java.awt.Color;
+import java.util.Random;
+
+import com.github.hanavan99.conwaygameoflife.model.Chunk;
 import com.github.hanavan99.conwaygameoflife.model.Game;
+import com.github.hanavan99.conwaygameoflife.model.Player;
 
 /**
  * The game model containing the challenge data
@@ -12,6 +17,16 @@ class Challenge extends Game {
 	 * Generates the challenge
 	 */
 	Challenge() {
-		// TODO
+		Random rand = new Random();
+		Player player = new Player("Challenge", Color.BLACK, 0, 0);
+		for ( int x = 0; x < NetworkConfig.CHALLENGE_CHUNK_SQRT; ++x ) {
+			for ( int y = 0; y < NetworkConfig.CHALLENGE_CHUNK_SQRT; ++y ) {
+				final long[] data = new long[64];
+				for ( int i = 0; i < 64; ++i ) {
+					data[i] = rand.nextLong();
+				}
+				getChunks().add(new Chunk(player, x, y, data, 0));
+			}
+		}
 	}
 }
