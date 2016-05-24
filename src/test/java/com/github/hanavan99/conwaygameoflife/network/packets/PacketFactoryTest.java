9e6d26bd3ec2862e3c0382cc5c27a6fdf2555f5b
Zach Deibert <zachdeibert@gmail.com>
2016-05-24 12:57:40 +0000

    Added packet id consistency test to PacketFactoryTest

--- src/test/java/com/github/hanavan99/conwaygameoflife/network/packets/PacketFactoryTest.java
+++ src/test/java/com/github/hanavan99/conwaygameoflife/network/packets/PacketFactoryTest.java
@@ -1,7 +1,15 @@
 package com.github.hanavan99.conwaygameoflife.network.packets;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -50,6 +58,41 @@ public class PacketFactoryTest {
 				Assert.assertEquals(i, PacketFactory.getId(pkt));
 			} catch ( IllegalArgumentException ex ) {
 				log.catching(Level.DEBUG, ex);
+			}
+		}
+	}
+
+	@Test
+	public void testConsistentIds() throws IOException, ClassNotFoundException {
+		File store = new File(new File("build"), "PacketFactoryTest.ser").getAbsoluteFile();
+		log.info("Storing PacketFactoryTest store in file {}", store);
+		Map<Integer, String> current = new HashMap<Integer, String>();
+		for ( int i = Byte.MIN_VALUE; i <= Byte.MAX_VALUE; ++i ) {
+			try {
+				IPacket pkt = PacketFactory.construct((byte) i);
+				current.put(i, pkt.getClass().getName());
+			} catch ( IllegalArgumentException ex ) {
+				log.catching(Level.DEBUG, ex);
+			}
+		}
+		if ( store.exists() ) {
+			try ( FileInputStream in = new FileInputStream(store) ) {
+				try ( ObjectInputStream obj = new ObjectInputStream(in) ) {
+					@SuppressWarnings("unchecked")
+					Map<Integer, String> old = (Map<Integer, String>) obj.readObject();
+					for ( int id : old.keySet() ) {
+						Assert.assertTrue(current.containsKey(id));
+						Assert.assertEquals(old.get(id), current.get(id));
+					}
+					if ( current.size() == old.size() ) {
+						return;
+					}
+				}
+			}
+		}
+		try ( FileOutputStream out = new FileOutputStream(store) ) {
+			try ( ObjectOutputStream obj = new ObjectOutputStream(out) ) {
+				obj.writeObject(current);
 			}
 		}
 	}
